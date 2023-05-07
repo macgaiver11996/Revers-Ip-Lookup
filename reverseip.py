7dc80e602d2cb4c963740a7e5b8a14207e9c3797
Macgaiver <126487835+macgaiver11996@users.noreply.github.com>
2023-05-07 19:21:00 +0600

    Add files via upload

---
+++ reverseip.py
@@ -0,0 +1,16 @@
+import requests, socket 
+
+website1 = input("Enter Your Target Site :")
+website = website1.replace("https://",'').replace("http://",'').replace("/",'')
+
+
+ip = socket.gethostbyname(website)
+api = f"https://api.hackertarget.com/reverseiplookup/?q={ip}"
+
+
+response = requests.get(api)
+
+if response.status_code == 200:
+  print ("response.text")
+else:
+  print ("Request Faild")
