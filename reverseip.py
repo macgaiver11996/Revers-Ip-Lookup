50337d35ed319310886ca45f4a73dc6be0d1fa4f
Macgaiver <126487835+macgaiver11996@users.noreply.github.com>
2023-10-06 01:19:22 +0600

    Update reverseip.py

--- reverseip.py
+++ reverseip.py
@@ -1,16 +1,32 @@
-import requests, socket 
+import os
+import requests
+import socket
 
-website1 = input("Enter Your Target Site :")
-website = website1.replace("https://",'').replace("http://",'').replace("/",'')
+filename = input("Enter the name : ")
+with open(filename, 'r') as file:
+    target_sites = file.read().splitlines()
 
+for to in target_sites:
+    site = to.replace("https://", "").replace("http://", "").rstrip("/")
+    try:
+        ip = socket.gethostbyname(site)
+    except socket.gaierror:
+        print(f"Invalid or unreachable URL: {to}")
+        continue
 
-ip = socket.gethostbyname(website)
-api = f"https://api.hackertarget.com/reverseiplookup/?q={ip}"
+    api = f"https://api.hackertarget.com/reverseiplookup/?q={ip}"
 
+    try:
+        response = requests.get(api)
+        if response.status_code == 200:
+            with open('website.txt', 'a') as mac:
+                mac.write(response.text + '\n')
+        else:
+            print(f"Request Failed for URL: {to}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request Exception for URL {to}: {e}")
+    except Exception as ex:
+        print(f"An error occurred for URL {to}: {ex}")
 
-response = requests.get(api)
+print("Reverse IP lookup completed.")
 
-if response.status_code == 200:
-  print ("ip")
-else:
-  print ("Request Faild")
